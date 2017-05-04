92b23a10d7f23e6fdabfcaee78692e653045674f
dingyong <dingyongjxnu@163.com>
2017-05-04 10:47:48 +0800

    修改注释

--- app/src/main/java/com/dingyong/view/custom/brokeLineView/BrokenLineTrendData.java
+++ app/src/main/java/com/dingyong/view/custom/brokeLineView/BrokenLineTrendData.java
@@ -7,7 +7,7 @@ import java.util.List;
  * BrokenLineTrendData
  */
 public class BrokenLineTrendData {
-    public List<String> mXLineDataList;//Xz轴的数据
+    public List<String> mXLineDataList;//X轴的数据
     public List<BrokenLineDimension> mDimensionList;//数据
     public List<Double> mYLineDataList;//y轴的数据
     public int mSelectColor;//X轴选中的的颜色
